975920bab326918f0445d02ba5a2948d1d684da1
Berkan <b.sevil326@gmail.com>
2025-01-18 16:23:49 +0300

    [DE-38] - adding log4j2.xml file

--- src/main/java/com/archisacademy/parking/services/concrete/VehicleServiceImpl.java
+++ src/main/java/com/archisacademy/parking/services/concrete/VehicleServiceImpl.java
@@ -9,19 +9,20 @@ import com.archisacademy.parking.exception.VehicleNotFoundException;
 import com.archisacademy.parking.model.Vehicle;
 import com.archisacademy.parking.repositories.VehicleRepository;
 import com.archisacademy.parking.services.abstracts.VehicleService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Service;
-
 import java.util.List;
 import java.util.stream.Collectors;
+
 
 @Service
 public class VehicleServiceImpl implements VehicleService {
 
     private final VehicleRepository vehicleRepository;
     private final ModelMapperService modelMapperService;
-    private final Logger logger = LoggerFactory.getLogger(VehicleServiceImpl.class);
+    private static final Logger logger = LogManager.getLogger(VehicleServiceImpl.class);
+
 
     public VehicleServiceImpl(VehicleRepository vehicleRepository, ModelMapperService modelMapperService) {
         this.vehicleRepository = vehicleRepository;
