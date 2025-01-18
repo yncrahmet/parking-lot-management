23f1f9f6cdf6f1c9c77b6b8b4a383740325a144b
mehmet fatih şimşek <147408701+fatihsimssek@users.noreply.github.com>
2025-01-18 23:33:34 +0300

    [DE-47] - Create API Documentation project infos added

--- src/main/java/com/archisacademy/parking/configuration/OpenAPIConfiguration.java
+++ src/main/java/com/archisacademy/parking/configuration/OpenAPIConfiguration.java
@@ -1,5 +1,7 @@
 package com.archisacademy.parking.configuration;
 
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
@@ -24,7 +26,19 @@ public class OpenAPIConfiguration {
         license.name("Apache 2.0");
         license.url("google.com");
 
+        Contact contact = new Contact();
+        contact.setName("Mehmet Fatih Şimşek");
+        contact.setUrl("https://github.com/fatihsimssek");
+        contact.setEmail("mm.fatihsimsek@gmail.com");
 
-        return new OpenAPI().servers(List.of(server));
+        Info info = new Info()
+                .title("Parking Management API")
+                .version("1.0.0")
+                .description("API for managing parking spaces and reservations.")
+                .termsOfService("https://parking.com/terms")
+                .license(license)
+                .contact(contact);
+
+        return new OpenAPI().servers(List.of(server)).info(info);
     }
 }
