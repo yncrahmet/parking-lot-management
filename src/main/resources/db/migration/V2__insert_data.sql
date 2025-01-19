2750e2c6c5c8eda008b7baff4ec5444c37447ba1
Ahmet Yeniçeri <yeniceriahmet02@gmail.com>
2025-01-19 13:32:08 +0300

    [DE-12] - Solved conflict

--- src/main/resources/db/migration/V2__insert_data.sql
+++ src/main/resources/db/migration/V2__insert_data.sql
@@ -2,6 +2,6 @@ INSERT INTO vehicles (registration_number, type, is_active)
 VALUES
     ('01ABC123', 'Car', true);
 
-INSERT INTO users (name, user_name, password, email, phone_number, active)
+INSERT INTO users (name, username, password, email, phone_number, active)
 VALUES
     ('Ahmet', 'ahmet', 'guvenlisifre', 'ahmet@mail.com', '1234567890', true);
