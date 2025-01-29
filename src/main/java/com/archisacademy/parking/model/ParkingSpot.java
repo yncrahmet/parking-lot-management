cc71e4d0546a2f8a338d25be55f8317bd6f94b09
gorkemiskeceli <mgiskec@gmail.com>
2025-01-29 21:29:17 +0300

    [DE-26] - entity has been updated

--- src/main/java/com/archisacademy/parking/model/ParkingSpot.java
+++ src/main/java/com/archisacademy/parking/model/ParkingSpot.java
@@ -1,16 +1,15 @@
 package com.archisacademy.parking.model;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
 import java.time.LocalDate;
 
 @Entity
-@Data
+@Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Table(name = "parking_spots")
