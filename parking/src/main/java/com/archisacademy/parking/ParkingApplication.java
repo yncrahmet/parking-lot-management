89a7297b3f887d4c8e67a80bafe0b4f6a7eb6123
mehmet fatih şimşek <147408701+fatihsimssek@users.noreply.github.com>
2025-02-05 21:03:19 +0300

    [DE-61] - Service Registry Eureka integrated

--- parking/src/main/java/com/archisacademy/parking/ParkingApplication.java
+++ parking/src/main/java/com/archisacademy/parking/ParkingApplication.java
@@ -2,8 +2,10 @@ package com.archisacademy.parking;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ParkingApplication {
 
 	public static void main(String[] args) {
