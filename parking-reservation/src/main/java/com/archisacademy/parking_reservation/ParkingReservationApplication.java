468bd20eb66b35597eba7c700b3b5c10103e080c
mehmet fatih şimşek <147408701+fatihsimssek@users.noreply.github.com>
2025-02-05 21:24:04 +0300

    [DE-61] - Service Registry EnableDiscoveryclient removed.

--- parking-reservation/src/main/java/com/archisacademy/parking_reservation/ParkingReservationApplication.java
+++ parking-reservation/src/main/java/com/archisacademy/parking_reservation/ParkingReservationApplication.java
@@ -2,10 +2,9 @@ package com.archisacademy.parking_reservation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
 
 @SpringBootApplication
-@EnableDiscoveryClient
 public class ParkingReservationApplication {
 
 	public static void main(String[] args) {
